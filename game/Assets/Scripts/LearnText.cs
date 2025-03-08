2b64747eafda40440e8044f253486d162e0b4e36
itsnotdanial <gkzv63@durham.ac.uk>
2025-03-08 17:14:57 +0000

    Added paper sfx and tweaked background image

--- game/Assets/Scripts/LearnText.cs
+++ game/Assets/Scripts/LearnText.cs
@@ -2,13 +2,33 @@ using UnityEngine;
 
 public class ToggleScrollView : MonoBehaviour
 {
-    [SerializeField] private GameObject scrollView; // Drag the Scroll View here in Inspector
+    [SerializeField] private GameObject scrollView;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        // Disable scroll view on start
+        scrollView.SetActive(false);
+
+        // Get the AudioSource from this GameObject
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource is missing! Make sure to add one to this GameObject.");
+        }
+    }
 
     void Update()
     {
         if (scrollView != null && Input.GetKeyDown(KeyCode.R))
         {
             scrollView.SetActive(!scrollView.activeSelf);
+
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
         }
     }
 }
