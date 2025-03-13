06a649aff3ac419d16a3509d4f8cc63feb9d6b90
itsnotdanial <gkzv63@durham.ac.uk>
2025-03-13 14:34:35 +0000

    added proximity based detection for scrollable text

--- game/Assets/Scripts/LearnText.cs
+++ game/Assets/Scripts/LearnText.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using TMPro;
 
 public class ToggleScrollView : MonoBehaviour
 {
-    [SerializeField] private GameObject scrollView;
+    [SerializeField] private GameObject scrollView;  // This is your scroll view UI element
+    [SerializeField] private Transform player;        // Player's Transform
+    [SerializeField] private Transform targetObject;  // Object to get close to
+    [SerializeField] private TextMeshProUGUI promptText; // TMP UI text
+    [SerializeField] private float interactionDistance = 3f;
+
     private AudioSource audioSource;
+    private bool isInRange = false;
 
     private void Start()
     {
-        // Disable scroll view on start
-        scrollView.SetActive(false);
+        scrollView.SetActive(false);           // Make sure scroll view is off at the start
+        promptText.gameObject.SetActive(false);  // Hide the prompt at the start
 
-        // Get the AudioSource from this GameObject
         audioSource = GetComponent<AudioSource>();
 
         if (audioSource == null)
@@ -19,16 +25,47 @@ public class ToggleScrollView : MonoBehaviour
         }
     }
 
-    void Update()
+    private void Update()
     {
-        if (scrollView != null && Input.GetKeyDown(KeyCode.R))
-        {
-            scrollView.SetActive(!scrollView.activeSelf);
+        float distance = Vector3.Distance(player.position, targetObject.position);
 
-            if (audioSource != null)
+        if (distance <= interactionDistance)
+        {
+            if (!isInRange)
             {
-                audioSource.Play();
+                isInRange = true;
+                promptText.gameObject.SetActive(true);  // Show the prompt when in range
             }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ToggleScrollViewVisibility();
+            }
+        }
+        else
+        {
+            if (isInRange)
+            {
+                isInRange = false;
+                promptText.gameObject.SetActive(false);  // Hide the prompt when out of range
+
+                // Auto-close the scroll view if it's open
+                if (scrollView.activeSelf)
+                {
+                    scrollView.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void ToggleScrollViewVisibility()
+    {
+        bool newState = !scrollView.activeSelf;
+        scrollView.SetActive(newState);
+
+        if (audioSource != null)
+        {
+            audioSource.Play();
         }
     }
 }
