2a4ab43ad6fe8ffeb13c4b744cd442e5a890336a
Toby Davis <pencilcaseman@gmail.com>
2025-03-18 21:42:41 +0000

    Stop player when showing UI

--- game/Assets/Scripts/PlayerMovement.cs
+++ game/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -380,5 +378,10 @@ public class PlayerMovement : MonoBehaviour
         {
             playerViewcone.RefreshLightPosition();
         }
+    }
+
+    public Rigidbody2D GetRigidBody()
+    {
+        return this.rb;
     }
 }
---
+++ game/Assets/Scripts/SkillsBuild/PlayerMovementStop.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovementStop : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement movement;
+    [SerializeField] private SkillsBuildUI questionUI;
+
+    public void Update()
+    {
+        if (this.questionUI.IsVisible())
+        {
+            this.movement.GetRigidBody().velocity = new Vector2(0.0f, 0.0f);
+        }
+    }
+}
